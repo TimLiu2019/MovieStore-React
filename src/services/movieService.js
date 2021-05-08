52a60a1af2419783d9eea6952159ab6e53e1c8b7
TimLiu2019 <qitingliu7@gmail.com>
2021-05-08 18:45:04 -0400

    saving the movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,10 +1,26 @@
 import http from "./httpService";
-import{apiUrl} from '../config.json'
-const apiEndpoint =apiUrl + "/movies";
+import { apiUrl } from "../config.json";
+const apiEndpoint = apiUrl + "/movies";
 export function getMovies() {
   return http.get(apiEndpoint);
 }
+export function getMovie(movieId) {
+  return http.get(apiEndpoint + "/" + movieId);
+}
+const getMovieUrl = id => {
+  return `${apiEndpoint}/${id}`;
+};
+export function saveMovie(movie) {
+  if (movie._id) {
+    // pass all movie key value to body
+    const body = { ...movie };
+    // delete movieId property
+    delete body.movieId;
+    return http.put(getMovieUrl(movie._id), body);
+  }
+  return http.post(apiEndpoint, movie);
+}
 
 export function deleteMovie(movieId) {
-  return http.delete(apiEndpoint + "/" + movieId);
+  return http.delete(getMovieUrl(movieId));
 }
