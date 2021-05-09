3de0d00bc3ea0d8addf28b923ec0d576666b26cf
TimLiu2019 <qitingliu7@gmail.com>
2021-05-08 21:11:18 -0400

    submitting the login form

---
+++ src/services/authService.js
@@ -0,0 +1,6 @@
+import http from "./httpService";
+import { apiUrl } from "../config.json";
+const apiEndpoint = apiUrl + "/auth";
+export function login(email, password) {
+  return http.post(apiEndpoint, { email, password });
+}
